3734ab9274de0a9f1e935f69793e8ed4de67a8b7
SyedKashanTayyab <kashan.tayyab@mutexsystemsltd.com>
2021-07-16 03:57:33 +0500

    'push'

--- src/component/DoctorScreens/DoctorHome.js
+++ src/component/DoctorScreens/DoctorHome.js
@@ -1,43 +1,59 @@
-import React, { createFactory, useEffect, useState } from 'react';
+import React, {createFactory, useEffect, useState} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   Image,
-  TouchableOpacity, Platform,
-  FlatList, PermissionsAndroid
+  TouchableOpacity,
+  Platform,
+  FlatList,
+  PermissionsAndroid,
 } from 'react-native';
 import colors from '../../constants/colors';
 import fonts from '../../constants/fonts';
-import { createDirectory } from '../utils/directory';
-import { askForPermission } from '../utils/permission';
+import {createDirectory} from '../utils/directory';
+import {askForPermission} from '../utils/permission';
 import {useSelector} from 'react-redux';
+import {api, headers} from '../Config/env';
 
-const DoctorHome = ({ navigation }) => {
-  const [dataSource, setDataSource] = useState([]);
-  const user = useSelector(state => state.user.user);
+const DoctorHome = ({navigation}) => {
+  const [Appointments, setAppointments] = useState([]);
+  const user = useSelector((state) => state.user.user);
 
   useEffect(() => {
-   
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then((response) => response.json())
-      .then((json) => setDataSource(json))
-      .catch((err) => console.log(err))
-      checkPermission()
+    getAppointments();
+    checkPermission();
+    //3447321161
   }, []);
 
-  const checkPermission = async () =>{
+  const checkPermission = async () => {
     try {
-      const res = await askForPermission()
-      if(res == 'granted'){
-        await createDirectory()
+      const res = await askForPermission();
+      if (res == 'granted') {
+        await createDirectory();
       }
-      
     } catch (error) {
       console.log(error);
     }
-  }
-
+  };
+  const getAppointments = () => {
+    fetch(`${api}doctor/show/doctor/schedule`, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify({
+        doctorId: user.data.user._id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson.status == 1) {
+          setAppointments(responseJson.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <View style={styles.container}>
       <View style={styles.uppercontainer}>
@@ -49,14 +65,14 @@ const DoctorHome = ({ navigation }) => {
             resizeMode="contain"
           />
         </TouchableOpacity>
-        <View style={[styles.rightview, { marginRight: 20 }]}>
+        <View style={[styles.rightview, {marginRight: 20}]}>
           <View style={styles.innerrightview}>
             <Text style={styles.nametext}>{user.data.user.name}</Text>
             <View style={styles.locationView}>
               <Image
                 source={require('../../../assets/Images/location.png')}
                 resizeMode="contain"
-                style={{ height: 18 }}
+                style={{height: 18}}
               />
               <Text style={styles.locationText}>Karachi</Text>
             </View>
@@ -71,7 +87,7 @@ const DoctorHome = ({ navigation }) => {
         </View>
       </View>
       <View style={styles.lowercontainer}>
-        <View style={{ flex: 1, justifyContent: 'center' }}>
+        <View style={{flex: 1, justifyContent: 'center'}}>
           <View style={styles.uppertextView}>
             <TouchableOpacity>
               <Text
@@ -101,94 +117,107 @@ const DoctorHome = ({ navigation }) => {
               paddingRight: 20,
               paddingTop: 10,
             }}>
-            <FlatList
-              data={dataSource}
-              showsHorizontalScrollIndicator={false}
-              horizontal={true}
-              renderItem={({ item }) => (
-                <TouchableOpacity style={styles.FlatListTouchButton}>
-                  <View style={{ flex: 1, flexDirection: 'row' }}>
-                    <View style={{ flex: 3 }}>
-                      <View style={[styles.rightview]}>
-                        <View
-                          style={[styles.DoctorImageView, { marginLeft: 10, borderColor: colors.Colors.Orange }]}>
-                          <Image
-                            source={require('../../../assets/Images/doctor.png')}
-                            resizeMode="stretch"
-                            style={styles.DoctorImage}
-                          />
-                        </View>
-                        <View style={styles.innerrightview}>
-                          <Text style={styles.nametext}>
-                            Syed Kashan Tayyab
-                          </Text>
+            {!!Appointments.length ? (
+              <FlatList
+                data={Appointments}
+                showsHorizontalScrollIndicator={false}
+                horizontal={true}
+                renderItem={({item}) => (
+                  <TouchableOpacity style={styles.FlatListTouchButton}>
+                    <View style={{flex: 1, flexDirection: 'row'}}>
+                      <View style={{flex: 3}}>
+                        <View style={[styles.rightview]}>
                           <View
                             style={[
-                              styles.locationView,
-                              { alignSelf: 'flex-start', marginLeft: 6 },
+                              styles.DoctorImageView,
+                              {
+                                marginLeft: 10,
+                                borderColor: colors.Colors.Orange,
+                              },
                             ]}>
                             <Image
-                              source={require('../../../assets/Images/orangelocation.png')}
-                              resizeMode="contain"
-                              style={{ height: 18 }}
+                              source={require('../../../assets/Images/doctor.png')}
+                              resizeMode="stretch"
+                              style={styles.DoctorImage}
                             />
-                            <Text
+                          </View>
+                          <View style={styles.innerrightview}>
+                            <Text style={styles.nametext}>{item.name}</Text>
+                            <View
                               style={[
-                                styles.locationText,
-                                { color: colors.Colors.Orange },
+                                styles.locationView,
+                                {alignSelf: 'flex-start', marginLeft: 6},
                               ]}>
-                              Karachi
-                            </Text>
+                              <Image
+                                source={require('../../../assets/Images/orangelocation.png')}
+                                resizeMode="contain"
+                                style={{height: 18}}
+                              />
+                              <Text
+                                style={[
+                                  styles.locationText,
+                                  {color: colors.Colors.Orange},
+                                ]}>
+                                Karachi
+                              </Text>
+                            </View>
                           </View>
                         </View>
                       </View>
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center' }}>
-                      <TouchableOpacity style={{ alignItems: 'center' }}>
-                        <Image
-                          source={require('../../../assets/Images/cross.png')}
-                          resizeMode="contain"
-                        />
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                  <View style={{ flex: 1 }}>
-                    <View
-                      style={[
-                        styles.belowButtonView,
-                        { paddingLeft: 5, paddingRight: 5 },
-                      ]}>
-                      <TouchableOpacity style={styles.belowButton}>
-                        <View
-                          style={[
-                            styles.bellowbuttoninnerview,
-                            { width: '15%' },
-                          ]}>
+                      <View style={{flex: 1, justifyContent: 'center'}}>
+                        <TouchableOpacity style={{alignItems: 'center'}}>
                           <Image
-                            source={require('../../../assets/Images/clock.png')}
+                            source={require('../../../assets/Images/cross.png')}
                             resizeMode="contain"
                           />
-                        </View>
-                        <View
-                          style={[
-                            styles.bellowbuttoninnerview,
-                            { width: '85%' },
-                          ]}>
-                          <Text
-                            style={[styles.belowButtonText, { marginLeft: 10 }]}>
-                            Sunday 6-Jan 2022
-                          </Text>
-                        </View>
-                      </TouchableOpacity>
+                        </TouchableOpacity>
+                      </View>
                     </View>
-                  </View>
-                </TouchableOpacity>
-              )}
-              keyExtractor={(item, index) => index.toString()}
-            />
+                    <View style={{flex: 1}}>
+                      <View
+                        style={[
+                          styles.belowButtonView,
+                          {paddingLeft: 5, paddingRight: 5},
+                        ]}>
+                        <TouchableOpacity style={styles.belowButton}>
+                          <View
+                            style={[
+                              styles.bellowbuttoninnerview,
+                              {width: '15%'},
+                            ]}>
+                            <Image
+                              source={require('../../../assets/Images/clock.png')}
+                              resizeMode="contain"
+                            />
+                          </View>
+                          <View
+                            style={[
+                              styles.bellowbuttoninnerview,
+                              {width: '85%'},
+                            ]}>
+                            <Text
+                              style={[
+                                styles.belowButtonText,
+                                {marginLeft: 10},
+                              ]}>
+                              {item.date} {item.timeSlot}
+                            </Text>
+                          </View>
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                )}
+                keyExtractor={(item, index) => index.toString()}
+              />
+            ) : (
+              <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+              <Text style={{fontFamily:fonts.fonts.PoppinsBold,fontSize:16}}>No Schedule</Text>
+                </View>
+            )}
           </View>
         </View>
-        <View style={{ flex: 1 }}>
+        <View style={{flex: 1}}>
           <Text style={styles.OtherappointmentText}>
             See Other Appointments
           </Text>
@@ -196,19 +225,19 @@ const DoctorHome = ({ navigation }) => {
             <TouchableOpacity
               style={[
                 styles.TouchButton,
-                { backgroundColor: '#FEE4D2', marginLeft: 20 },
+                {backgroundColor: '#FEE4D2', marginLeft: 20},
               ]}>
               <Image
                 source={require('../../../assets/Images/pastapointment.png')}
                 resizeMode="contain"
-                style={{ height: '40%' }}
+                style={{height: '40%'}}
               />
               <Text
                 style={{
                   color: '#FD872C',
                   fontSize: 16,
                   fontFamily: fonts.fonts.PoppinsRegular,
-                  textAlign:'center'
+                  textAlign: 'center',
                 }}>
                 Past Appointments
               </Text>
@@ -216,19 +245,19 @@ const DoctorHome = ({ navigation }) => {
             <TouchableOpacity
               style={[
                 styles.TouchButton,
-                { backgroundColor: '#FFDDDD', marginLeft: 10, marginRight: 20 },
+                {backgroundColor: '#FFDDDD', marginLeft: 10, marginRight: 20},
               ]}>
               <Image
                 source={require('../../../assets/Images/cancelapointment.png')}
                 resizeMode="contain"
-                style={{ height: '40%' }}
+                style={{height: '40%'}}
               />
               <Text
                 style={{
                   color: '#FF4238',
                   fontSize: 16,
                   fontFamily: fonts.fonts.PoppinsRegular,
-                  textAlign:'center'
+                  textAlign: 'center',
                 }}>
                 Cancel Appointments
               </Text>
@@ -236,12 +265,12 @@ const DoctorHome = ({ navigation }) => {
           </View>
           <View style={styles.belowButtonView}>
             <TouchableOpacity style={styles.belowButton}>
-              <View style={[styles.bellowbuttoninnerview, { width: '85%' }]}>
-                <Text style={[styles.belowButtonText, { marginLeft: 10 }]}>
+              <View style={[styles.bellowbuttoninnerview, {width: '85%'}]}>
+                <Text style={[styles.belowButtonText, {marginLeft: 10}]}>
                   Payments
                 </Text>
               </View>
-              <View style={[styles.bellowbuttoninnerview, { width: '15%' }]}>
+              <View style={[styles.bellowbuttoninnerview, {width: '15%'}]}>
                 <Image
                   source={require('../../../assets/Images/rightarrow.png')}
                   resizeMode="contain"
@@ -378,6 +407,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginRight: 20,
-    marginLeft: 20
+    marginLeft: 20,
   },
 });
