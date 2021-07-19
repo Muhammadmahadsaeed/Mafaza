c1c018873118fd442de54b3a2d9b6fb48a55edf0
SyedKashanTayyab <kashan.tayyab@mutexsystemsltd.com>
2021-07-19 16:21:03 +0500

    push

--- src/component/Config/env.js
+++ src/component/Config/env.js
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 // export const api = 'http://34.200.187.81:3001/';
 // export const APOLLO_GRAPHQL = 'http://34.200.187.81:4002/graphql'
 // export const APOLLO_SOCKET = 'http://34.200.187.81:4002/graphql' //server
 
-export const api = 'http://192.168.161.138:3000/';
+export const api = 'http://192.168.50.127:3000/';
 export const APOLLO_GRAPHQL = 'http://192.168.161.138:4001/graphql'
 export const APOLLO_SOCKET = 'http://192.168.161.138:4001/graphql' 
-=======
-export const api = 'http://192.168.50.190:3000/';
-export const APOLLO_GRAPHQL = 'http://192.168.0.103:4001/graphql'
-export const APOLLO_SOCKET = 'http://192.168.0.103:4001/graphql'
->>>>>>> aaebef911890c4596967b2119e3b597f29ea5e8e
 
 export const headers = {
     'Content-Type': 'application/json'
--- src/component/PatientScreens/DoctorAppointment.js
+++ src/component/PatientScreens/DoctorAppointment.js
@@ -1,131 +1,100 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from 'react';
-=======
-import React, { useState,useEffect } from 'react';
->>>>>>> aaebef911890c4596967b2119e3b597f29ea5e8e
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
-  Image,
+  Button,
   TextInput,
   ScrollView,
   KeyboardAvoidingView,
-  ActivityIndicator, Platform
+  ActivityIndicator,
+  Platform,
 } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import colors from '../../constants/colors';
 import fonts from '../../constants/fonts';
-import { api, headers } from '../Config/env';
-import { useSelector } from 'react-redux';
+import {api, headers} from '../Config/env';
+import {useSelector} from 'react-redux';
+import DatePicker from 'react-native-datepicker';
+import { color } from 'react-native-reanimated';
 
-const DoctorProfile = ({ navigation }) => {
+const DoctorProfile = ({navigation}) => {
   const [loading, setLoading] = useState(false);
-  const [name, setName] = useState('')
-  const [contactNum, setContactNum] = useState('')
-  const [userSelectedDay, setUserSelectedDay] = useState('')
-  const [userSelectedTime, setUserSelectedTime] = useState('')
+  const [name, setName] = useState('');
+  const [contactNum, setContactNum] = useState('');
+  const [userSelectedDay, setUserSelectedDay] = useState('');
+  const [userSelectedTime, setUserSelectedTime] = useState('');
   const [message, setmessage] = useState('');
   const [errortext, seterrortext] = useState('');
-  const [day, setDay] = useState(false) //open dropdown for days
-  const [time, setTime] = useState(false) //open dropdown for time
-  const [showDays, setShowDays] = useState([]) //arr for showing days
-  const [selectTime, setSelectedTime] = useState([]) //arr for showing selected time against day
-  const [data, setData] = useState([]) //arr for get all data from api
+  const [day, setDay] = useState(false); //open dropdown for days
+  const [time, setTime] = useState(false); //open dropdown for time
+  const [showDays, setShowDays] = useState([]); //arr for showing days
+  const [selectTime, setSelectedTime] = useState([]); //arr for showing selected time against day
+  const [data, setData] = useState([]); //arr for get all data from api
   const user = useSelector((state) => state.user.user);
-  const DoctorData = navigation.getParam('doctorData')
-  const [day, setday] = useState(false);
-  const [time, settime] = useState(false);
+  const DoctorData = navigation.getParam('doctorData');
   const [Appointments, setAppointments] = useState([]);
+  const [date, setDate] = useState();
 
-  useEffect(() => {
-    getAppointments();
-  }, []);
-
-  const getAppointments = () => {
-    fetch(`${api}appointment/getfree/slots`, {
+  const getTimeSlot = async (Dates) => {
+    setDate(Dates);
+    setData([])
+    await fetch(`${api}appointment/check_date_wise_slot`, {
       method: 'POST',
       headers: headers,
       body: JSON.stringify({
-        doctorId: DoctorData._id,
+        // doctorId: DoctorData._id,
+        doctorId: '60efc2076627c50efc476baa',
+        date: Dates,
       }),
     })
-      .then((response) => response.json())
-      .then((responseJson) => {
-        if (responseJson.status == 1) {
-          setAppointments(responseJson.data);
-          console.log(responseJson.data,'-------------')
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  const openDayDropDown = () => {
-    setday(true);
-    settime(false);
-  };
-  const openTimeDropDown = () => {
-    setday(false);
-    settime(true);
-  };
-
-  useEffect(() => {
-    getTimeSlot()
-  }, [])
-
-  const getTimeSlot = async () => {
-    await fetch(`${api}appointment/getfree/slots`, {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify({
-        doctorId: DoctorData._id
-      })
-    })
-      .then(res => res.json())
+      .then((res) => res.json())
       .then((json) => {
-        console.log(json);
-        setData(json.data)
-        const day = json.data.map((res) => {
-          return { label: res.day, value: res.day }
-        })
-        setShowDays(day)
+        setData(json.data);
+        getDayFromUser(json.data[0].day)
+        
+        // const day = json.data.map((res) => {
+        //   return {label: res.day, value: res.day};
+        // });
+        // setShowDays(day);
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   const getDayFromUser = (item) => {
-    setUserSelectedDay(item)
-    const time = []
+    // setUserSelectedDay(item);
+    console.log(data);
+    const time = [];
     data.filter((res) => {
       if (res.day == item) {
-        res.slots.map(result => {
+        res.slots.map((result) => {
           let slots = {
-            label: result.from + " to " + result.to,
-            value: result.from + " to " + result.to
-          }
-          time.push(slots)
-        })
+            label: result.from + ' to ' + result.to,
+            value: result.from + ' to ' + result.to,
+          };
+          time.push(slots);
+        });
       }
-    })
-    setSelectedTime(time)
-  }
+    });
+    // console.log(time);
+    setSelectedTime(time);
+  };
 
   const submitAppointment = () => {
-    seterrortext('')
-    const slot = userSelectedTime.split(' to ')
+    seterrortext('');
+    const slot = userSelectedTime.split(' to ');
     let slotObj = {
       from: slot[0],
-      to: slot[1]
-    }
+      to: slot[1],
+    };
 
     // if(name == '' && contactNum == '' && userSelectedDay == ''){
 
     // }
     // if (timeSlot != '') {
     //   if (consultation_purpose != '') {
-    setLoading(true)
+    setLoading(true);
     fetch(`${api}appointment/add`, {
       method: 'POST',
       headers: headers,
@@ -144,10 +113,10 @@ const DoctorProfile = ({ navigation }) => {
         console.log(responseJson);
         if (responseJson.status == 1) {
           setLoading(false);
-          setName('')
-          setContactNum('')
-          setmessage('')
-          alert("Your appointment have been booked")
+          setName('');
+          setContactNum('');
+          setmessage('');
+          alert('Your appointment have been booked');
         } else {
           setLoading(false);
           seterrortext('Ckeck your Internet connection');
@@ -168,62 +137,21 @@ const DoctorProfile = ({ navigation }) => {
     // }
   };
   const openDayDropDown = () => {
-    setDay(true)
-    setTime(false)
+    setDay(true);
+    setTime(false);
     // setSelectedTime([])
-  }
+  };
   const openTimeDropDown = () => {
-    setTime(true)
-    setDay(false)
-  }
+    setTime(true);
+    setDay(false);
+  };
+
   return (
     <View style={styles.container}>
-      <ScrollView keyboardShouldPersistTaps="handled" showsVerticalScrollIndicator={false}>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        showsVerticalScrollIndicator={false}>
         <KeyboardAvoidingView enabled>
-<<<<<<< HEAD
-=======
-          <View style={styles.dropDownView}>
-            <View style={{...(Platform.OS == 'ios' && {zIndex: 10 }),width:'48%'}}>
-              <DropDownPicker
-                items={[
-                  { label: 'Mafaza Patient', value: 'Mafaza Patient' },
-                  { label: 'Personal Patient', value: 'Personal Patient' },
-                  { label: 'Both', value: 'Both' },
-                ]}
-                placeholder="Select Day"
-                selectedLabelStyle={{ color: 'black' }}
-                placeholderStyle={{ color: '#9EA0A4' }}
-                style={styles.dropDownStyle}
-                isVisible={day}
-                onOpen={() => openDayDropDown()}
-                onClose={() => setday(false)}
-                itemStyle={styles.dropDownItemStyle}
-                dropDownStyle={{ backgroundColor: '#fafafa' }}
-                // onChangeItem={(item) => setPatientType(item.value)}
-              />
-            </View>
-            <View style={{...(Platform.OS == 'ios' && {zIndex: 10 }),width:'48%'}}>
-              <DropDownPicker
-                items={[
-                  { label: 'Mafaza Patient', value: 'Mafaza Patient' },
-                  { label: 'Personal Patient', value: 'Personal Patient' },
-                  { label: 'Both', value: 'Both' },
-                ]}
-                placeholder="Select Time Slot"
-                selectedLabelStyle={{ color: 'black' }}
-                isVisible={time}
-                onOpen={() => openTimeDropDown()}
-                onClose={() => settime(false)}
-                placeholderStyle={{ color: '#9EA0A4' }}
-                style={styles.dropDownStyle}
-                itemStyle={styles.dropDownItemStyle}
-                dropDownStyle={{ backgroundColor: '#fafafa' }}
-                // onChangeItem={(item) => setPatientType(item.value)}
-              />
-            </View>
-          </View>
-
->>>>>>> aaebef911890c4596967b2119e3b597f29ea5e8e
           <View style={styles.inputContainer}>
             <TextInput
               style={styles.inputStyle}
@@ -237,13 +165,12 @@ const DoctorProfile = ({ navigation }) => {
               onChangeText={(text) => setContactNum(text)}
             />
           </View>
-
           <View style={styles.inputContainer}>
             <TextInput
               style={styles.inputStyle}
               placeholder="Enter your name"
               autoCapitalize="none"
-              keyboardType='ascii-capable'
+              keyboardType="ascii-capable"
               returnKeyType="next"
               value={name}
               underlineColorAndroid="#f000"
@@ -251,8 +178,33 @@ const DoctorProfile = ({ navigation }) => {
               onChangeText={(text) => setName(text)}
             />
           </View>
-          <View style={styles.dropDownView}>
-            <View style={{ ...(Platform.OS == 'ios' && { zIndex: 10 }), width: '48%' }}>
+
+          <DatePicker
+            style={styles.DatePicker}
+            date={date}
+            minDate={new Date()}
+            mode="date"
+            confirmBtnText="OK"
+            cancelBtnText="Cancel"
+            onDateChange={(date)=>getTimeSlot(date)}
+            format="YYYY-MM-DD"
+            customStyles={{
+              dateIcon: {
+                position: 'absolute',
+                left: 5,
+                marginLeft: 0,
+              },
+              dateInput: {
+                borderWidth: 0,
+                marginLeft: 40,
+                alignItems: 'flex-start',
+              },
+            }}
+          />
+
+          {/* <View style={styles.dropDownView}>
+            <View
+              style={{...(Platform.OS == 'ios' && {zIndex: 10}), width: '48%'}}>
               <DropDownPicker
                 items={showDays}
                 placeholder="Select Day"
@@ -263,11 +215,12 @@ const DoctorProfile = ({ navigation }) => {
                 isVisible={day}
                 onOpen={() => openDayDropDown()}
                 onClose={() => setDay(false)}
-                dropDownStyle={{ backgroundColor: '#fafafa' }}
+                dropDownStyle={{backgroundColor: '#fafafa'}}
                 onChangeItem={(item) => getDayFromUser(item.value)}
               />
-            </View>
-            <View style={{ ...(Platform.OS == 'ios' && { zIndex: 10 }), width: '48%' }}>
+            </View> */}
+            <View
+              style={{...(Platform.OS == 'ios' && {zIndex: 10}), width: '100%',marginTop:20}}>
               <DropDownPicker
                 items={selectTime}
                 placeholder="Select Time"
@@ -278,18 +231,18 @@ const DoctorProfile = ({ navigation }) => {
                 onOpen={() => openTimeDropDown()}
                 onClose={() => setTime(false)}
                 itemStyle={styles.dropDownItemStyle}
-                dropDownStyle={{ backgroundColor: '#fafafa' }}
+                dropDownStyle={{backgroundColor: '#fafafa'}}
                 onChangeItem={(item) => setUserSelectedTime(item.value)}
               />
             </View>
-          </View>
+          {/* </View> */}
 
-          <View style={[styles.inputContainer, { height: 150 }]}>
+          <View style={[styles.inputContainer, {height: 150}]}>
             <TextInput
               style={styles.textArea}
               placeholder="Enter Message (Optional)"
               autoCapitalize="none"
-              keyboardType='ascii-capable'
+              keyboardType="ascii-capable"
               returnKeyType="next"
               multiline={true}
               numberOfLines={10}
@@ -302,7 +255,8 @@ const DoctorProfile = ({ navigation }) => {
           <View>
             <Text style={styles.txtstyle}>{errortext}</Text>
           </View>
-          <TouchableOpacity activeOpacity={0.8}
+          <TouchableOpacity
+            activeOpacity={0.8}
             onPress={() => submitAppointment()}
             style={styles.Btndesign}>
             {loading ? (
@@ -334,15 +288,15 @@ const styles = StyleSheet.create({
   inputStyle: {
     fontFamily: fonts.fonts.PoppinsRegular,
     fontSize: 16,
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
   },
   textArea: {
     fontFamily: fonts.fonts.PoppinsRegular,
     fontSize: 16,
     paddingHorizontal: 10,
     textAlign: 'justify',
-    justifyContent: "flex-start",
-    textAlignVertical: 'top'
+    justifyContent: 'flex-start',
+    textAlignVertical: 'top',
   },
   Btndesign: {
     backgroundColor: colors.Colors.Orange,
@@ -365,22 +319,15 @@ const styles = StyleSheet.create({
     marginTop: 10,
     textAlign: 'center',
   },
-<<<<<<< HEAD
   dropDownView: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-=======
-  dropDownView:{
-    flex:1,
-    flexDirection:'row',
-    justifyContent:'space-between',
->>>>>>> aaebef911890c4596967b2119e3b597f29ea5e8e
     marginTop: 20,
   },
   dropDownContainer: {
     width: '50%',
-    flexDirection: 'column'
+    flexDirection: 'column',
   },
   dropDownStyle: {
     backgroundColor: '#fafafa',
@@ -392,9 +339,21 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
   },
   dropDownSelectedLabelStyle: {
-    color: 'gray', fontSize: 12, fontFamily: fonts.fonts.PoppinsRegular,
+    color: 'gray',
+    fontSize: 12,
+    fontFamily: fonts.fonts.PoppinsRegular,
   },
   dropDownPlaceholderStyle: {
-    fontSize: 12, fontFamily: fonts.fonts.PoppinsRegular, color: 'gray'
-  }
+    fontSize: 12,
+    fontFamily: fonts.fonts.PoppinsRegular,
+    color: 'gray',
+  },
+  DatePicker: {
+    borderColor: colors.Colors.Blue,
+    borderRadius: 7,
+    borderWidth: 1,
+    marginTop: 20,
+    width: '100%',
+    height:50
+  },
 });
